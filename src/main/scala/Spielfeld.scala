dd6c07acc43ce56e39daca9b53f3e397fd2a2242
JuliusConradt <julius.conradt@gmail.com>
2024-10-24 12:46:11 +0200

    Spielfeld Object funktioniert

---
+++ src/main/scala/Spielfeld.scala
@@ -0,0 +1,18 @@
+object Spielfeld {
+  val red = "\u001B[31m"
+  val yellow = "\u001B[33m"
+  val green = "\u001B[32m"
+  val blue = "\u001B[34m"
+  val white = "\u001B[0m"
+  val mesh = ("+" + ("-") * 5) * 12 + "+"
+  val ryline = "|  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  | 10  | 11  | 12  | [ ] |"
+  val gbline = "| 12  | 11  | 10  |  9  |  8  |  7  |  6  |  5  |  4  |  3  |  2  | [ ] |"
+
+  def main(args: Array[String]): Unit = {
+    println("Welcome to Quixx")
+    printf("%s%s%n%s%n%s%n", red, mesh, ryline, mesh)
+    printf("%s%s%n%s%n%s%n", yellow, mesh, ryline, mesh)
+    printf("%s%s%n%s%n%s%n", green, mesh, gbline, mesh)
+    printf("%s%s%n%s%n%s%n%s", blue, mesh, gbline, mesh, white)
+  }
+}
